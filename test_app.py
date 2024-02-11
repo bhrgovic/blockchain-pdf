9dc2cb3ee52c963e20b957fbd73ed4c6e5d55257
Bruno Hrgovic <bruno.hrgovic@gmail.com>
2024-02-11 01:38:47 +0100

    test from terminal function

--- test_app.py
+++ test_app.py
@@ -1,55 +1,26 @@
-import unittest
-from flask_testing import TestCase
-from blueprints.app import app, file_blockchain
-from urllib.parse import quote
+import requests
 
-class TestSearchEndpoint(TestCase):
-    def create_app(self):
-        app.config['TESTING'] = True
-        return app
+def send_pdf_to_server(url, file_path, email):
+    """
+    Sends a PDF file to the server.
 
-    def test_add_pdf(self):
-        # Create a JSON object for the PDF
-        pdf = {
-            "pdf_name": "test_pdf",
-            "pdf_data": "test_data"
-        }
+    :param url: URL of the endpoint (e.g., 'http://<server-ip>:<port>/add_pdf')
+    :param file_path: Path to the PDF file to upload.
+    :param email: Email associated with the PDF.
+    """
+    files = {'file': open(file_path, 'rb')}
+    data = {'email': email}
 
-        # Add the PDF to the blockchain
-        file_blockchain.add_pdf(pdf)
+    response = requests.post(url, files=files, data=data)
 
-        # Search for the PDF
-        pdf_data = file_blockchain.search_pdf(pdf["pdf_name"])
+    return response
 
-        # Check that the PDF data is correct
-        self.assertEqual(pdf_data, pdf["pdf_data"])
+# Example usage
+if __name__ == "__main__":
+    url = 'http://localhost:5000/add_pdf'
+    file_path = '/blank.pdf'  # Update this path
+    email = 'pi3@test.com'
 
-
-
-    def test_search_pdf_found(self):
-        # Add a PDF to the blockchain
-        pdf = {"pdf_name": "test_pdf", "pdf_data": "test_data"}
-        file_blockchain.add_pdf(pdf)
-
-        # Search for the PDF
-        response = self.client.get('/search?pdf_name=' + quote(pdf["pdf_name"]))
-        print(response.data)  # Print the response data
-
-        # Check that the status code is 200
-        self.assertEqual(response.status_code, 200)
-
-        # Check that the response data is correct
-        self.assertEqual(response.json, {"status": "found", "pdf_data": "test_data"})
-
-    def test_search_pdf_not_found(self):
-        # Search for a PDF that does not exist
-        response = self.client.get('/search?pdf_name=' + quote("non_existent_pdf"))
-        print(response.data)  # Print the response data
-
-        # Check that the status code is 404
-        self.assertEqual(response.status_code, 400)
-
-        # Check that the response data is correct
-        self.assertEqual(response.json, {"status": "not found", "message": "PDF with name 'non_existent_pdf' not found in the blockchain or transaction pool."})
-if __name__ == '__main__':
-    unittest.main()
+    response = send_pdf_to_server(url, file_path, email)
+    print(f"Response Status Code: {response.status_code}")
+    print(f"Response Text: {response.text}")
